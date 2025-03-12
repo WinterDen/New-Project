3c5e27a68f08ffe6304dd32d74ba06387cc3f476
Winter Den <ladowofsla@gmail.com>
2025-03-12 01:07:53 -0300

    Changed to a simple rock, paper and scissors game.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello World')
+import random
+
+computerList = ['Rock','Paper','Scissors']
+
+computerC = (random.choice(computerList))
+
+humanC = input("What's your move?:\n"
+               "1- Rock\n"
+               "2- Paper\n"
+               "3- Scissors")
+
+if humanC == 'Rock' and computerC == 'Paper':
+    print('You lose')
+elif humanC == 'Rock' and computerC == 'Scissors':
+    print('You win')
+elif humanC == 'Paper' and computerC == 'Rock':
+    print('You Win')
+elif humanC == 'Paper' and computerC == 'Scissors':
+    print('You Lose')
+elif humanC == 'Scissors' and computerC == 'Rock':
+    print('You Lose')
+elif humanC == 'Scissors' and computerC == 'Paper':
+    print('You Win')
+else:
+    print('Draw')
